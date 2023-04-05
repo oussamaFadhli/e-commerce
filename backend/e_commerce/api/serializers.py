042abe6eebc547ec2f308aa108d027fbb242cad3
GorJani <www.oussama55aw5@gmail.com>
2023-04-06 00:53:00 +0100

    user api completed

--- backend/e_commerce/api/serializers.py
+++ backend/e_commerce/api/serializers.py
@@ -1,6 +1,10 @@
 from rest_framework import serializers
-from .models import *
-
-pass
+from .models import User
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email',
+                  'first_name', 'last_name', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
